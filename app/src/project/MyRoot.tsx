89d22d319295188b081f3b41047c46f67d0e499c
NilsBaumgartner1994 <33295329+NilsBaumgartner1994@users.noreply.github.com>
2023-05-21 22:03:26 +0200

    Update MyRoot.tsx

--- app/src/project/MyRoot.tsx
+++ app/src/project/MyRoot.tsx
@@ -8,7 +8,8 @@ export const MyRoot: FunctionComponent = (props) => {
   const [navigationHistory, setNavigationHistory] = useSynchedJSONState(RequiredSynchedStates.navigationHistory)
 
   return (
-    <View>
+    <View style={{visibility: "hidden"}}>
+      <a href="[https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2FFireboltCasters%2Fschule-rechenmeister](https://github.com/FireboltCasters/schule-rechenmeister)"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2FFireboltCasters%2Fschule-rechenmeister&label=Besucher&countColor=%23263759" /></a>
     </View>
   );
 }
